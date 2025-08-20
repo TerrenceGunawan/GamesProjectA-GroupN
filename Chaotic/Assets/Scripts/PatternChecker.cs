46c05d86eb2c4bb4d99dbf820a2ba0a3049b37a7
TerrenceGunawan <135194407+TerrenceGunawan@users.noreply.github.com>
2025-08-21 05:51:32 +1000

    UI for puzzle
    Working on the code but not fully fleshed out. thinking about how to approach

---
+++ Chaotic/Assets/Scripts/PatternChecker.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PatternChecker : MonoBehaviour
+{
+
+    [SerializeField] private Toggle A1, B1, C1, D1, A2, B2, C2, D2, A3, B3, C3, D3, A4, B4, C4, D4;
+    
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void CheckPattern(bool toggle)
+    {
+        if (toggle)
+        {
+            if
+        }
+    }
+}
