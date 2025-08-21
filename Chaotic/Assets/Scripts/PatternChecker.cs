47e3946c596a4d298e7c4089c36877d879f61046
Darren-SUG <d4rr3nsugihartono@gmail.com>
2025-08-21 13:08:25 +1000

    changed ui a little

--- Chaotic/Assets/Scripts/PatternChecker.cs
+++ Chaotic/Assets/Scripts/PatternChecker.cs
@@ -1,26 +1,26 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class PatternChecker : MonoBehaviour
-{
+// public class PatternChecker : MonoBehaviour
+// {
 
-    [SerializeField] private Toggle A1, B1, C1, D1, A2, B2, C2, D2, A3, B3, C3, D3, A4, B4, C4, D4;
+//     [SerializeField] private Toggle A1, B1, C1, D1, A2, B2, C2, D2, A3, B3, C3, D3, A4, B4, C4, D4;
     
 
-    void Start()
-    {
+//     void Start()
+//     {
 
-    }
+//     }
 
-    void Update()
-    {
+//     void Update()
+//     {
 
-    }
+//     }
 
-    public void CheckPattern(bool toggle)
-    {
-        if (toggle)
-        {
-            if
-        }
-    }
-}
+//     public void CheckPattern(bool toggle)
+//     {
+//         if (toggle)
+//         {
+//             if
+//         }
+//     }
+// }
